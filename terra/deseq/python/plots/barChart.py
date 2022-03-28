05e94c873dcea3c2eebcb645b6ef7c9a31d221a3
Andrew E. Davidson <Andy@SantaCruzAnalytics.com>
2022-03-28 15:57:33 -0700

    early checkin xtick labels need work, x off by 1 tick

--- terra/deseq/python/plots/barChart.py
+++ terra/deseq/python/plots/barChart.py
@@ -10,9 +10,11 @@ __date__ = '2022-03-26'
 __updated__ = '2022-03-26'
 __user_name__ = 'Andrew E. Davidson'
 
+from matplotlib import pyplot as plt
+import numpy as np
 import pandas as pd
 from plots.barChartCommandLine  import BarChartCommandLine
-from matplotlib import pyplot as plt
+from plots.utilities import MatPlotLibUtilities
 
 ########################################################################
 def main( inComandLineArgsList=None ):
@@ -27,26 +29,89 @@ def main( inComandLineArgsList=None ):
         
     print(cli.args)
     
-    figureWidthInInches = 4
-    figureHeightInInches = 3
-    fig = plt.figure(figsize=(figureWidthInInches,figureHeightInInches))    
     
     #
     # load data
     #
     dataDF = pd.read_csv( cli.args.inputFile )
     columnNames = dataDF.columns
-    # print(columnNames)
-    
     xLabel = columnNames[0]
-    # print(xLabel)
-    x = dataDF.loc[:, xLabel]
-    # print(x)
-    
     yLabel = columnNames[1]
-    # print(yLabel)
-    y = dataDF.loc[:,yLabel]
+    dataDF = dataDF.sort_values(yLabel)
+    print(dataDF)
+    
+    # xLabel will be something like lung, kidney, brain
+    xLabels = dataDF.loc[:, xLabel].to_list()
+    print(xLabels)
+    x = np.arange(len(xLabels))  # the label locations
+
+    
+    # y values are the number of sample in each tissue type, i.e. int
+    y = dataDF.loc[:,yLabel].to_list()
     # print(y)
+    
+    #
+    # plot set up
+    #
+    mplu = MatPlotLibUtilities()
+    mplu.loadStyle()
+        
+    figureWidthInInches = 8
+    figureHeightInInches = 3
+    fig = plt.figure(figsize=(figureWidthInInches,figureHeightInInches))   
+
+    panelHeightInInches = figureHeightInInches #2
+    bottomRelativeSize = 0.0 #0.2
+    panelWidthInInches = figureWidthInInches #2
+    leftRelativeSize = 0.0 #0.2
+    barChartPanel = mplu.createPanel( fig,
+                        panelWidthInInches, panelHeightInInches,
+                        leftRelativeSize, bottomRelativeSize )
+    
+    #
+    # config labels and axis
+    #
+    barChartPanel.set_xlabel( xLabel )
+    barChartPanel.set_xticks(x) # , xLabels
+    barChartPanel.set_xticklabels(xLabels, rotation = 45, fontsize=4)
+
+    barChartPanel.set_ylabel( yLabel)
+    # make sure all points are plotted
+    yMin = np.floor( np.min( y ) )
+    yMax = np.ceil( np.max( y ) )
+    barChartPanel.set_ylim( yMin, yMax )
+    
+    #
+    #
+    # 
+    numBars = len(y)
+    barChartPanel.bar(x, y, tick_label=xLabels)
+            
+    title = cli.args.title 
+    if title:
+        barChartPanel.set_title(title)
+        
+    #barChartPanel.set_xticks(x, labels)
+
+# x = np.arange(len(labels))  # the label locations
+# width = 0.35  # the width of the bars
+#
+# fig, ax = plt.subplots()
+# rects1 = ax.bar(x - width/2, men_means, width, label='Men')
+# rects2 = ax.bar(x + width/2, women_means, width, label='Women')
+#
+# # Add some text for labels, title and custom x-axis tick labels, etc.
+# ax.set_ylabel('Scores')
+# ax.set_title('Scores by group and gender')
+# ax.set_xticks(x, labels)
+# ax.legend()
+#
+# ax.bar_label(rects1, padding=3)
+# ax.bar_label(rects2, padding=3)
+
+    outputFile = cli.args.outputFile
+    fig.savefig(outputFile, dpi=300, bbox_inches='tight')
+    print("saved plot: {}".format(outputFile))    
     
 ########################################################################
 if __name__ == '__main__':
