c0b49937f9ca6bc3611f5c8ebca4c1516171f84a
Andrew E. Davidson <aedavids@ucsc.edu>
2022-05-04 15:00:03 -0700

    clean up

--- terra/deseq/bin/cutAndPaste.sh
+++ terra/deseq/bin/cutAndPaste.sh
@@ -5,7 +5,21 @@
 # are the sample names
 #
 
-set -x 
+if [ "$#" -lt 1 ]; then
+    echo "ERROR arguments are not correct"
+    echo "Usage: $0 outputFile"
+    echo "create a tsv file from all the salmon quant.sf files in the current directory"
+    echo "will uncompress if in gz format"
+    echo " example $0 myMatrix; will produce a file myMatrix.tsv"
+    exit 1
+fi
+
+outputFile=$1
+
+# ref: https://gist.github.com/vncsna/64825d5609c146e80de8b1fd623011ca 
+#set -euxo pipefail
+set -x
+
 
 # check if file is compressed or not
 for f in `ls *quant.sf*`;
@@ -31,19 +45,19 @@ done
 # ENST00000450305.2|ENSG00000223972.5|OTTHUMG00000000961.2|OTTHUMT00000002844.2|DDX11L1-201|DDX11L1|632|transcribed_unprocessed_pseudogene|	632	466.0000.000000	0.000
 
 # cut the numReads column and remove the header line
-mkdir cut.out
+mkdir -p cut.out
 for q in `ls *.quant.sf`;
 do
     printf "\n****** $q"
     sampleName=`echo $q | cut -d . -f 1`
-    sed '1d' $q | cut -f 5 | head -n 5 > cut.out/${sampleName}
+    sed '1d' $q | cut -f 5 > cut.out/${sampleName}
 done
 
 #
 # get the transcript names
 #
 quantFile=`ls *.quant.sf | head -n 1`
-sed '1d' ${quantFile} | cut -f 1 | head -n 5 > names.txt
+sed '1d' ${quantFile} | cut -f 1  > names.txt
 
 #
 # combine into a single table
@@ -65,4 +79,4 @@ printf "\n" >> header.txt
 # strip out all the new lines to convert column into a row
 #sed -e 's/\n//g' column.txt > rowHeader.txt
 
-cat header.txt tmpTable.tsv > table.tsv
+cat header.txt tmpTable.tsv > "${outputFile}.tsv"
