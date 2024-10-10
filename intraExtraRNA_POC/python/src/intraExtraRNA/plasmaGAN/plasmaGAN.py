72fd71fc75009fec263214974809b2dc14400156
Andrew E. Davidson <aedavids@ucsc.edu>
2024-10-10 10:30:32 -0700

    load data from local cache

--- intraExtraRNA_POC/python/src/intraExtraRNA/plasmaGAN/plasmaGAN.py
+++ intraExtraRNA_POC/python/src/intraExtraRNA/plasmaGAN/plasmaGAN.py
@@ -5,11 +5,16 @@
 #
 '''
 public functions 
+    loadCountData()
    
 '''
 
 import logging
+import os.path
 import pandas as pd
+
+from analysis.utilities import loadList
+from analysis.utilities import saveList
 
 from intraExtraRNA.elifeUtilities import loadCounts
 from intraExtraRNA.elifeUtilities import loadMetaData
@@ -17,11 +22,89 @@ from intraExtraRNA.elifeUtilities import selectFeatures
 
 logger = logging.getLogger(__name__)
 
+################################################################################
 def loadCountData(
+    localCacheDir : str,
     HUGO_featuresNames : list[str]
 ) -> tuple[pd.DataFrame, pd.DataFrame, list[str], list[str], pd.DataFrame]:
     '''
-    TODO
+    given a list of HUGO gene biomarkers loads corresponding elife normalized
+    count data.
+
+    arguments:
+        localCacheDir : str
+            if not none
+                if not in local cache 
+                    load from /private and store in cache
+
+        HUGO_featuresNames : list[str]
+
+    returns :
+        (XDF, metaDF, elifeLungGenes, missingElifeGenes, mapDF)
+
+        XDF : index = sample id, columns = [gene_loci_name], values = counts
+
+        metaDF : columns = [sample_id, diagnosis]
+
+        elifeLungGenes : list of ensembl gene names
+
+        missingElifeGenes
+
+        mapDF : columns = [HUGO_v35	ENSG_v35	ENSG_v39]
+    '''
+    logger.info( "BEGIN" )
+
+    XDFPath               = os.path.join( localCacheDir, "XDF.csv" )
+    metaDFPath            = os.path.join( localCacheDir, "metaDF.csv" )
+    elifeLungGenesPath    = os.path.join( localCacheDir, "elifeLungGenes.txt" )
+    missingElifeGenesPath = os.path.join( localCacheDir, "missingElifeGenes.txt" )
+    mapDFPath             = os.path.join( localCacheDir, "mapDF.csv" )
+
+    if localCacheDir == None :
+        logger.info( f'localCacheDir undefined loading from private')
+        retTuple = loadCountDataImpl( HUGO_featuresNames )
+
+    else :
+        if os.path.isfile(XDFPath):
+            logger.info( f'loading from {localCacheDir}' )
+            # read everything from local cache
+            XDF = pd.read_csv( XDFPath )
+            metaDF = pd.read_csv( metaDFPath )
+            mapDF = pd.read_csv( mapDFPath )
+            
+            elifeLungGenes = loadList( elifeLungGenesPath )
+            missingElifeGenes = loadList( missingElifeGenesPath )
+
+            retTuple = (XDF, metaDF, elifeLungGenes, missingElifeGenes, mapDF)
+
+        else :
+            # load from private and store in cache
+            logger.info( f'localCacheDir is empty, load from private')
+            retTuple = loadCountDataImpl( HUGO_featuresNames )
+            XDF, metaDF, elifeLungGenes, missingElifeGenes, mapDF = retTuple
+
+            # save
+            os.makedirs( localCacheDir, exist_ok=True )
+
+            XDF.to_csv( XDFPath)
+            metaDF.to_csv( metaDFPath )
+
+            saveList( elifeLungGenesPath, elifeLungGenes, isSingleItemLine=False )
+            saveList( missingElifeGenesPath, missingElifeGenes, isSingleItemLine=False )
+
+            mapDF.to_csv( mapDFPath )
+
+
+    logger.info( "END" )
+    return retTuple
+
+################################################################################
+def loadCountDataImpl(
+    HUGO_featuresNames : list[str]
+) -> tuple[pd.DataFrame, pd.DataFrame, list[str], list[str], pd.DataFrame]:
+    '''
+    given a list of HUGO gene biomarkers loads corresponding elife normalized
+    count data.
 
     arguments:
         HUGO_featuresNames : list[str]
@@ -40,6 +123,9 @@ def loadCountData(
         mapDF : columns = [HUGO_v35	ENSG_v35	ENSG_v39]
 
     '''
+    logger.info( "BEGIN" )
+
+
     transposedCountsDF = loadCounts()
     logger.info( f'transposedCountsDF.shape : {transposedCountsDF.shape}' )
     logger.info( f'{transposedCountsDF.iloc[0:5, 0:5]}' )
@@ -59,9 +145,10 @@ def loadCountData(
 
     # where did this Gene come from? Why is it missing
     logger.warning(f'AEDWIP where did this come form and why is it missing? ENSG00000182378.15_PAR_Y')
-    biomakerGenes = [s for s in elifeLungGenes if s != 'ENSG00000182378.15_PAR_Y']
+    biomakerGenes = [s for s in elifeLungGenes if s != "ENSG00000182378.15_PAR_Y"]
     XDF = transposedCountsDF.loc[:, biomakerGenes]
     logger.info( f'XDF.shape : {XDF.shape}' )
     logger.info( f'XDF.iloc[0:5, 0:3] : \n{XDF.iloc[0:5, 0:3]}' )
 
+    logger.info( "END" )
     return (XDF, metaDF, elifeLungGenes, missingElifeGenes, mapDF)
